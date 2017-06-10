c368bed6692abbf7af363bf5dff4d0923b72d61d
GStewartN <stewart.nathang@gmail.com>
2017-06-09 18:55:48 -0700

    fix indentation, attempt hide result css

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,25 @@ var pingPong = function(number) {
     return "Error: Please enter numbers only";
   }
 
-
 for (var i = 1; i <= number; i += 1) {
-  $("ul").append("<li>" + i + "</li>");
-  if (i % 3 === 0) {
-    $("ul").append("<li>" + "Ping" + "</li>");
-  } 
+  if (i % 3 === 0 && i % 5 === 0) {
+    $("ul").append("<li>" + "PingPong" + "</li>");
+  } else if (i % 5 === 0) {
+      $("ul").append("<li>" + "Pong" + "</li>");
+  } else if (i % 3 === 0) {
+      $("ul").append("<li>" + "Ping" + "</li>");
+  } else {
+      $("ul").append("<li>" + i + "</li>");
+  }
 }
 
 };
-
-
-
-
-
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
-    $("ul").text(result);
+    $("#result").show("ul").text(result);
   });
 });
