7fcf7f461a5d333531306ec2746f0bfb0fa9ba20
GStewartN <stewart.nathang@gmail.com>
2017-06-09 18:08:27 -0700

    count entered number from 1-(entered #)

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,13 @@ var pingPong = function(number) {
     return "Error: Please enter numbers only";
   }
 
-var newNumber = "";
+
 
 for (var i = 1; i <= number; i += 1) {
-  if (i % 3 === 0) {
-    return newNumber.concat("Ping");
-  }
+  $("ul").append("<li>" + i + "</li>");
 }
 
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -44,8 +20,8 @@ for (var i = 1; i <= number; i += 1) {
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-    var number = ($("input#number").val());
+    var number = parseInt($("input#number").val());
     var result = pingPong(number);
-    $("#result").text(result);
+    $("ul").text(result);
   });
 });
