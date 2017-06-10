5f87ccdb61f947977546c1e4c0631f8b0e2a0f82
GStewartN <stewart.nathang@gmail.com>
2017-06-10 09:31:02 -0700

    test toggle results function

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,24 @@ var pingPong = function(number) {
     return "Error: Please enter numbers only";
   }
 
-for (var i = 1; i <= number; i += 1) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    $("ul").append("<li>" + "PingPong" + "</li>");
-  } else if (i % 5 === 0) {
-      $("ul").append("<li>" + "Pong" + "</li>");
-  } else if (i % 3 === 0) {
-      $("ul").append("<li>" + "Ping" + "</li>");
-  } else {
-      $("ul").append("<li>" + i + "</li>");
-  }
-}
+  for (var i = 1; i <= number; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      $("ul").append("<li>" + "PingPong" + "</li>");
+    } else if (i % 5 === 0) {
+        $("ul").append("<li>" + "Pong" + "</li>");
+      } else if (i % 3 === 0) {
+        $("ul").append("<li>" + "Ping" + "</li>");
+      } else {
+        $("ul").append("<li>" + i + "</li>");
+      }
+    }
 
 };
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
+    //$("#result").toggle("ul").text(result);
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
     $("#result").show("ul").text(result);
